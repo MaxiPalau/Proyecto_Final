4f1c1965187a3834f84aba72b03128e8cbed1487
Maximiliano Palau <90297730+maxi-j-p@users.noreply.github.com>
2022-06-17 14:12:10 -0300

    Agregado registro

--- Proyecto_Final/django_base/urls.py
+++ Proyecto_Final/django_base/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, login_view
+from .views import index, login_view, logout_view, register_view
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -23,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name = 'index'),
     path('productos/', include('productos.urls')),
-    path('login', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Proyecto_Final/django_base/views.py
+++ Proyecto_Final/django_base/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 
 def index(request):
    return render(request, 'index.html', context = {})
@@ -34,3 +34,30 @@ def login_view(request):
       form = AuthenticationForm()
       context = {'form':form}
       return render(request, 'auth/login.html', context=context)
+
+def logout_view(request):
+   logout(request)
+   return redirect('index')
+
+def register_view(request):
+   boton = 0
+   if request.method == 'POST':
+      form = UserCreationForm(request.POST)
+      
+      if form.is_valid():
+         form.save()
+         mensaje = 'Usuario creado con éxito.'
+         boton = 1
+         context = {'message': mensaje, 'boton':boton}
+         return render(request, 'auth/register.html', context=context)
+      else:
+         f_error = form.errors
+         for key in f_error:
+            error = f_error[key]
+         form = UserCreationForm()
+         context = {'errors':error, 'form':form, 'boton':boton}
+         return render(request, 'auth/register.html', context=context) 
+   else:
+      form = UserCreationForm()
+      context = {'form':form, 'boton':boton}
+      return render(request, 'auth/register.html', context=context)
